71530ec1bbb37ebaef3cbf2b8853150efe8f579f
Gabriele Cannata <gcannata@gmail.com>
2022-09-26 22:42:49 +0200

    Using instapaper library

--- src/Globals.d.ts
+++ src/Globals.d.ts
@@ -1,3 +1,4 @@
 declare module "*.module.css";
 declare module "*.module.scss";
 declare module "*.svg";
+declare module "instapaper";
--- src/InstapaperProvider.ts
+++ src/InstapaperProvider.ts
@@ -1,4 +1,18 @@
+// https://oauth.net/core/1.0a/
+// https://www.instapaper.com/api
+// https://www.npmjs.com/package/oauth-signature
+// https://github.com/robertklep/node-instapaper
 
+import Instapaper from "instapaper";
+
+const CONSUMER_KEY = "";
+const CONSUMER_SECRET = "";
+
+export async function loginUser(username: string, password: string) {
+    const client = Instapaper(CONSUMER_KEY, CONSUMER_SECRET);
+    const {toke, secret} =  await client.setUserCredentials(username, password).authenticate();
+    return {toke, secret};
+}
 
 export function authorize() {
     // TODO
